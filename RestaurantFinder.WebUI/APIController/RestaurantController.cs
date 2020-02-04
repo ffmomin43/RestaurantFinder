f03966965b33570e8ccf4344d65e94030ea85c7a
UMAIR <umairshahbaz44@gmail.com>
2020-02-04 19:26:27 +0500

    change webui api Restaurant

--- RestaurantFinder.WebUI/APIController/RestaurantController.cs
+++ RestaurantFinder.WebUI/APIController/RestaurantController.cs
@@ -17,31 +17,47 @@ namespace RestaurantFinder.WebUI.APIController
 
         private readonly Lazy<IRestaurantService> restaurantService;
         private readonly Lazy<IUsersService> usersService;
-        private readonly Lazy<IRestaurantsImagesService> restaurantsImages;
+        private readonly Lazy<IRestaurantsImagesService> restaurantsImage;
         private readonly Lazy<ICategoryMasterService> categoryMasterService;
+        private readonly Lazy<IPictureService> pictureService;
         private readonly Lazy<ILoggerFacade<RestaurantController>> logger;
-        
+
         public RestaurantController(
-            Lazy<IRestaurantService> restaurantService, 
+            Lazy<IRestaurantService> restaurantService,
             Lazy<ILoggerFacade<RestaurantController>> logger,
             Lazy<ICategoryMasterService> categoryMasterService,
-                        Lazy<IUsersService> usersService          
+            Lazy<IRestaurantsImagesService> restaurantsImage,
+             Lazy<IPictureService> pictureService,
+                        Lazy<IUsersService> usersService
             )
         {
             this.restaurantService = restaurantService;
             this.categoryMasterService = categoryMasterService;
+            this.restaurantsImage = restaurantsImage;
             this.usersService = usersService;
+            this.pictureService = pictureService;
             this.logger = logger;
         }
 
         // GET: api/Restaurant
         [Route("api/Restaurant")]
-        public IEnumerable<Restaurant> Get()
+        public IEnumerable<RestaurantimagesVm> Get()
         {
-            
-            return restaurantService.Value.GetAll();
-        }
 
+            {
+                var get = from n in restaurantService.Value.GetAll()
+                          join s in restaurantsImage.Value.GetAll() on n.ID equals s.RestaurantId
+                          join p in pictureService.Value.GetAll() on s.PictureId equals p.ID
+                          select new RestaurantimagesVm
+                          {
+                              AddressLine1 = n.AddressLine1,
+                              RestaurantsImages = p.url,
+
+
+                          };
+                return get;
+            }
+        }
         // GET: api/Restaurant/5
         public Restaurant Get(Guid id)
         {
