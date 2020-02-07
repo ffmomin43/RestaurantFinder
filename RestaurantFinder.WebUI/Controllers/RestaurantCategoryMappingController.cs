65b969bc5afe1ce5357922252dec1dcc94dcefa2
UMAIR <umairshahbaz44@gmail.com>
2020-02-08 03:23:32 +0500

    create RestaurantCategoryMapping controller

---
+++ RestaurantFinder.WebUI/Controllers/RestaurantCategoryMappingController.cs
@@ -0,0 +1,33 @@
+﻿using RestaurantFinder.BusinessLogic.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantFinder.WebUI.Controllers
+{
+    public class RestaurantCategoryMappingController : Controller
+    {
+
+        
+        private readonly Lazy<ICategoryMasterService> categoryMasterService;
+        private readonly Lazy<IRestaurantService> restaurantService;
+
+        public RestaurantCategoryMappingController(
+            Lazy<ICategoryMasterService> categoryMasterService,
+             Lazy<IRestaurantService> restaurantService
+            )
+        {
+            this.categoryMasterService = categoryMasterService;
+            this.restaurantService = restaurantService;
+        }
+
+        // GET: RestaurantCategoryMapping
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
