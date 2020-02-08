debb5d04ab95f66303af3def8e98b6a403dd5352
Fahad Momin <ffmomin43@gmail.com>
2020-02-08 18:19:07 +0530

    HomebannerImage model and repo and migration added

---
+++ RestaurantFinder.Models/HomeBannerImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantFinder.Models
+{
+    public class HomeBannerImage: BaseModel
+    {
+        public string Url { get; set; }
+
+        public string HeadingText { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
