378f3ae70f7f4c52279057fc3cd00b362b54c316
UMAIR <umairshahbaz44@gmail.com>
2020-02-13 00:06:35 +0500

    Add HomeBannerImages Controller

---
+++ RestaurantFinder.WebUI/Controllers/HomeBannerImagesController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantFinder.WebUI.Controllers
+{
+    public class HomeBannerImagesController : Controller
+    {
+        // GET: HomeBannerImages
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HomeBannerImages/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeBannerImages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeBannerImages/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeBannerImages/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeBannerImages/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeBannerImages/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeBannerImages/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
