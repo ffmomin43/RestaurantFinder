5225450895cf21ad4076401d2aa39df80dd349dc
UMAIR <umairshahbaz44@gmail.com>
2020-02-19 01:48:53 +0500

    restaurantSlot Service Create

---
+++ RestaurantFinder.BusinessLogic/Impl/RestaurantSlotService.cs
@@ -0,0 +1,54 @@
+﻿using RestaurantFinder.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantFinder.BusinessLogic.Impl
+{
+   public class RestaurantSlotService
+    {
+
+        private readonly Lazy<IRestaurantSlotRepository> restaurantSlotRepository;
+
+        public RestaurantSlotService(Lazy<IRestaurantSlotRepository> restaurantSlotRepository)
+        {
+            this.restaurantSlotRepository = restaurantSlotRepository;
+        }
+
+        public void Add(Models.RestaurantSlot entity)
+        {
+
+            restaurantSlotRepository.Value.Add(entity);
+        }
+
+        public void Delete(Models.RestaurantSlot entity)
+        {
+            restaurantSlotRepository.Value.Delete(entity);
+        }
+
+        public void Edit(Models.RestaurantSlot entity)
+        {
+            restaurantSlotRepository.Value.Edit(entity);
+        }
+
+        public IQueryable<Models.RestaurantSlot> FindBy(Expression<Func<Models.RestaurantSlot, bool>> predicate)
+        {
+            return restaurantSlotRepository.Value.FindBy(predicate);
+        }
+
+
+        public IQueryable<Models.RestaurantSlot> GetAll()
+        {
+            return restaurantSlotRepository.Value.GetAll();
+        }
+
+
+        public void Save()
+        {
+            restaurantSlotRepository.Value.Save();
+        }
+    }
+}
